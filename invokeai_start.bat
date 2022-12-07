65b17fb7aae53779864c2f6cc7c09344e50f54c9
Cyrus Chan <cyrus@TELI-C02NVAUSFY14.local>
2022-12-07 15:12:43 +0800

    Added env variable in startup script

--- invokeai_start.bat
+++ invokeai_start.bat
@@ -1,5 +1,7 @@
 set PATH=C:\windows\system32
 set PATH=C:\anaconda\Scripts;%PATH%
+set INVOKEAI_ROOT=C:\anaconda\projects\InvokeAI
+set INVOKEAI_OUTDIR=C:\anaconda\projects\InvokeAI\outputs
 
 call C:\anaconda\condabin\conda activate invokeai
 C:\anaconda\envs\invokeai\python.exe scripts/invoke.py --web --host 0.0.0.0 --port 9090 --nsfw_checker --sampler=ddim --steps=20 --root="C:\anaconda\projects\InvokeAI" --outdir="C:\anaconda\projects\InvokeAI\outputs"
